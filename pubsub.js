ea8f99287556c7a11cc13c1f02612143877b5630
Benjamin A. Petersen <benjaminapetersen@Benjamins-MacBook-Pro.local>
2015-05-11 22:47:30 -0400

    Convert factory to provider to provide configuration for publish fallback function
    - user can choose severity of publish errors
    	- at moment, fallback is to console.warn the subscription path and data
    	- user can override with an empty function, or a more severe & throw an error

--- pubsub.js
+++ pubsub.js
@@ -1,58 +1,126 @@
+'use strict';
+
 angular.module('pubsub', [
 
 ])
 
-.factory('pubsub', function() {
-    var subscriptions = {};
 
-    return {
-        subscribe: function(subscription, subscriber) {
-            if(!subscriptions[subscription]) {
-                subscriptions[subscription] = [];
-            }
+.provider('pubsub', [
+    function() {
+        var subscriptions = {};
 
-            // adds the callback to the array & returns the index for deletion later
-            var index = (subscriptions[subscription].push(subscriber) -1);
-
-            // return an unsubscribe function
-            return {
-                unsubscribe: function() {
-                    delete subscriptions[subscription][index];
-                }
-            }
-
-        },
-        publish: function(subscription, data) {
-
-            // TODO: convert to a provider & provide configuration
-            // to set a severity to failure to publish.
-            // Default should probably be at least a warning. Perhaps
-            // set a level or provide a callback function...?
-            if(!subscriptions[subscription]) {
-                return;
-            }
-            subscriptions[subscription].forEach(function(subscriber) {
-                subscriber(data || {});
-            });
-
-        },
-        utils: {
-            // get a list of all subscriptions
-            getAll: function() {
-                return subscriptions;
-            },
-            // clear a single subscription
-            clear: function(subscription) {
-                if(subscription && subscriptions[subscription]) {
-                    delete subscriptions[subscription];
-                }
-            },
-            // clean out the whole pub/sub mechanism
-            clean: function() {
-                subscriptions = {};
-            }
+        var publishFallback = function(subscription, data) {
+            console.warn('no subscriptions for', subscription, data);
         }
-    };
 
-});
+        this.setPublishFallback = function(fn) {
+            publishFallback = fn;
+        }
+
+        this.$get = [
+            '$log',
+            function($log) {
+                return {
+                    subscribe: function(subscription, subscriber) {
+                        if(!subscriptions[subscription]) {
+                            subscriptions[subscription] = [];
+                        }
+
+                        // adds the callback to the array & returns the index for deletion later
+                        var index = (subscriptions[subscription].push(subscriber) -1);
+
+                        // return an unsubscribe function
+                        return {
+                            unsubscribe: function() {
+                                delete subscriptions[subscription][index];
+                            }
+                        }
+
+                    },
+                    publish: function(subscription, data) {
+
+                        if(!subscriptions[subscription]) {
+                            return publishFallback(subscription, data);
+                        }
+                        subscriptions[subscription].forEach(function(subscriber) {
+                            subscriber(data || {});
+                        });
+
+                    },
+                    utils: {
+                        // get a list of all subscriptions
+                        getAll: function() {
+                            return subscriptions;
+                        },
+                        // clear a single subscription
+                        clear: function(subscription) {
+                            if(subscription && subscriptions[subscription]) {
+                                delete subscriptions[subscription];
+                            }
+                        },
+                        // clean out the whole pub/sub mechanism
+                        clean: function() {
+                            subscriptions = {};
+                        }
+                    }
+                };
+            }
+        ]
+    }
+])
+
+
+// .factory('pubsub', function() {
+//     var subscriptions = {};
+
+//     return {
+//         subscribe: function(subscription, subscriber) {
+//             if(!subscriptions[subscription]) {
+//                 subscriptions[subscription] = [];
+//             }
+
+//             // adds the callback to the array & returns the index for deletion later
+//             var index = (subscriptions[subscription].push(subscriber) -1);
+
+//             // return an unsubscribe function
+//             return {
+//                 unsubscribe: function() {
+//                     delete subscriptions[subscription][index];
+//                 }
+//             }
+
+//         },
+//         publish: function(subscription, data) {
+
+//             // TODO: convert to a provider & provide configuration
+//             // to set a severity to failure to publish.
+//             // Default should probably be at least a warning. Perhaps
+//             // set a level or provide a callback function...?
+//             if(!subscriptions[subscription]) {
+//                 return;
+//             }
+//             subscriptions[subscription].forEach(function(subscriber) {
+//                 subscriber(data || {});
+//             });
+
+//         },
+//         utils: {
+//             // get a list of all subscriptions
+//             getAll: function() {
+//                 return subscriptions;
+//             },
+//             // clear a single subscription
+//             clear: function(subscription) {
+//                 if(subscription && subscriptions[subscription]) {
+//                     delete subscriptions[subscription];
+//                 }
+//             },
+//             // clean out the whole pub/sub mechanism
+//             clean: function() {
+//                 subscriptions = {};
+//             }
+//         }
+//     };
+
+// });
 
